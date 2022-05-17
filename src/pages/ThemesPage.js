a420ed7b79a4fa4c1027ccca1b5fe0fefe00ae9c
Yen-Peng <46236142+Yen-Peng@users.noreply.github.com>
2022-05-18 02:17:04 +0800

    Add basic themes selection and set word to guess

--- src/pages/ThemesPage.js
+++ src/pages/ThemesPage.js
@@ -1,10 +1,22 @@
 import React from "react";
 import './ThemesPage.css';
+import { Button } from "react-bootstrap";
+import movies from '../assets/movies.json';
+import sports from '../assets/sports.json';
 
 function ThemesPage() {
+    const [word, setWord] = React.useState('');
+
     return (
         <div className="container">
             <p>Choose a theme</p>
+            <Button onClick={() => setWord(
+                movies[Math.floor(Math.random() * movies.length)].word
+            )}>Movies</Button>
+            <Button onClick={() => setWord(
+                sports[Math.floor(Math.random() * sports.length)].word
+            )}>Sports</Button>
+            {word}
         </div>
     )
 }
