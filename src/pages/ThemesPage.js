1fa7c4eba3ab4ac238c025f5e7fb81b1b16fcaa2
joanne-ch <JOANNECH001@e.ntu.edu.sg>
2022-05-18 15:44:54 +0800

    added 15 words for each theme

--- src/pages/ThemesPage.js
+++ src/pages/ThemesPage.js
@@ -2,9 +2,15 @@ import React from "react";
 import './ThemesPage.css';
 import { Button } from "react-bootstrap";
 import movies from '../assets/movies.json';
-import sports from '../assets/sports.json';
+import sports from'../assets/sports.json';
+import countries from '../assets/countries.json';
+import animals from '../assets/animals.json';
+import foods from '../assets/foods.json';
+
 
 function ThemesPage() {
+    
+
     const [wordToGuess, setWordToGuess] = React.useState('');
     const [theme, setTheme] = React.useState('');
 
@@ -16,10 +22,15 @@ function ThemesPage() {
     }
 
     return (
-        <div className="container">
+        <div>
             <p>Choose a theme</p>
-            <Button onClick={() => setWordAndTheme('Movies', movies)}>Movies</Button>
+
+            <Button onClick={() => setWordAndTheme('Movies', movies) }>Movies</Button>
             <Button onClick={() => setWordAndTheme('Sports', sports)}>Sports</Button>
+            <Button onClick={() => setWordAndTheme('Food', foods)}>Food</Button>
+            <Button onClick={() => setWordAndTheme('Animals', animals)}>Animals</Button>
+            <Button onClick={() => setWordAndTheme('Countries', countries)}>Countries</Button>
+
             {theme}<br/>
             {wordToGuess}
         </div>
