7d3f207cf0df9ece85a0619ca50706522880ba23
joanne-ch <JOANNECH001@e.ntu.edu.sg>
2022-05-18 21:11:45 +0800

    clicking theme button leads to game
    This reverts commit a5848719fed69c200367c617c90423e073944708, reversing
    changes made to 101c315b17396fa12703b743d86d87727fe8d2ad.

--- src/features/username/username.js
+++ src/features/username/username.js
@@ -1,38 +1,32 @@
 import React, { useState } from "react";
 import "./username.css";
-import { Button } from "react-bootstrap";
+import { Button } from 'react-bootstrap';
 
 function Username({ type = "text", label }) {
   const [name, setName] = useState("");
-  const [isValid, setIsValid] = useState(true);
 
   function handleChange(e) {
     setName(e.target.value);
   }
 
   function handleSubmit(e) {
-    e.preventDefault();
-    const uname = document.getElementById("username").value;
-    if (uname.length > 15) {
-      setIsValid(false);
-      return;
-    } else if (uname === "") {
-      // Set name to 'Guest' if blank
-      sessionStorage.setItem("Username", "Guest");
-    } else {
-      sessionStorage.setItem("Username", uname);
-    }
-    setIsValid(true);
-    // Change page to 'themes' upon clicking submit
-    window.location.replace("themes");
+      e.preventDefault();
+      const uname = document.getElementById('username').value;
+      if (uname === "") {
+          // Set name to 'Guest' if blank
+          sessionStorage.setItem('Username', 'Guest');
+      } else {
+        sessionStorage.setItem('Username', uname);
+      }
+      // Change page to 'themes' upon clicking submit
+      window.location.replace("themes");
   }
 
   return (
     <div className="input-container">
-      <form autocomplete="off" onSubmit={handleSubmit}>
-        <input id="username" type={type} value={name} onChange={handleChange} />
+      <form onSubmit={handleSubmit}>
+        <input id='username' type={type} value={name} onChange={handleChange} />
         <label className={name && "filled"}>{label}</label>
-        {isValid ? undefined : <p id="wrongpass">15 characters max!</p>}
         <Button type="submit">Start</Button>
       </form>
     </div>
--- src/pages/ThemesPage.js
+++ src/pages/ThemesPage.js
@@ -1,13 +1,12 @@
 import React from "react";
 import './ThemesPage.css';
-import { Button } from "react-bootstrap";
 import movies from '../assets/movies.json';
 import sports from'../assets/sports.json';
 import countries from '../assets/countries.json';
 import animals from '../assets/animals.json';
 import foods from '../assets/foods.json';
-
 import styles from './ThemesPage.module.css';
+
 
 function ThemesPage() {
     
@@ -21,17 +20,42 @@ function ThemesPage() {
             array[Math.floor(Math.random() * array.length)].word
         );
     }
+    
+    function handleClick(themes){
+        if (themes === 1){
+            setWordAndTheme('Movies', movies);
+            window.location.replace("hangman");} 
+        else if (themes === 2){
+            setWordAndTheme('Sports', sports);
+            window.location.replace("hangman");} 
+        else if (themes === 3){
+            setWordAndTheme('Food', foods);
+            window.location.replace("hangman");} 
+        else if (themes === 4){
+            setWordAndTheme('Animals', animals);
+            window.location.replace("hangman");} 
+        else if (themes === 5){
+            setWordAndTheme('Countries', countries);
+            window.location.replace("hangman");} 
+        else
+            window.location.replace("ErrorPage");
+            
+        
+    }
 
     return (
+        
         <div className={styles.container}>
-                <button onClick={() => setWordAndTheme('Movies', movies) } className={styles.btn}>MOVIES</button>
-                <button onClick={() => setWordAndTheme('Sports', sports) } className={styles.btn}>SPORTS</button>
-                <button onClick={() => setWordAndTheme('Food', foods) } className={styles.btn}>FOODS</button>
-                <button onClick={() => setWordAndTheme('Animals', animals) } className={styles.btn}>ANIMALS</button>
-                <button onClick={() => setWordAndTheme('Countries', countries) } className={styles.btn}>COUNTRIES</button>
+                <button onClick={() => handleClick(1) } className={styles.btn}>MOVIES</button>
+                <button onClick={() => handleClick(2) } className={styles.btn}>SPORTS</button>
+                <button onClick={() => handleClick(3) } className={styles.btn}>FOODS</button>
+                <button onClick={() => handleClick(4) } className={styles.btn}>ANIMALS</button>
+                <button onClick={() => handleClick(5) } className={styles.btn}>COUNTRIES</button>
             {/* {theme}<br/>
             {wordToGuess}    */}
         </div>
+
+
     )
 }
 export default ThemesPage;
