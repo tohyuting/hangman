8bd3bc11f43fcc2fff4a753349a4ffe57911e8b5
yutingtoh <tohyuting@gmail.com>
2022-05-25 16:50:54 +0800

    Add bulb icon to hint button

--- src/features/display/Display.js
+++ src/features/display/Display.js
@@ -165,7 +165,7 @@ export function Display() {
           <div className={ styles.textStatus }>
             <h2 className={ styles.h2 }>Theme: {getTheme} </h2>
             <button className={styles.hintButton} onClick = {() => alert(giveHint(letterShown))}> 
-            Get Hint
+            Get Hint <blink><i class="fa fa-lightbulb-o"></i></blink>
             </button>
           </div>
 
